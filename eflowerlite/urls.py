7e677bc7d8f04e4798a98ccf6bea065d724ee998
Renelle Tigue <titingrenelle@gmail.com>
2017-03-28 03:46:18 +0800

    create layout template, set up navbar, configure static files

--- eflowerlite/urls.py
+++ eflowerlite/urls.py
@@ -13,11 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 from . import views
 
 urlpatterns = [
+    url(r'^vrpages/', include('vrpages.urls')),
     url(r'^admin/', admin.site.urls),
-    url(r'^$', views.hello_world),
+    url(r'^$', views.home_page),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
--- vrpages/views.py
+++ vrpages/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def send_email(request):
+    return render(request, 'vrpages/send_email.html')
