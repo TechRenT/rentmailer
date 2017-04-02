c5ca4e323880e9a1909ff102beef0b668a4ebf96
Renelle Tigue <titingrenelle@gmail.com>
2017-04-02 16:24:27 +0800

    set database credentials as environment variables

--- eflowerlite/deploy_settings/__init__.py
+++ eflowerlite/deploy_settings/__init__.py
@@ -13,9 +13,9 @@ SECRET_KEY = get_env_variable("SECRET_KEY")
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'eflowerlite_prod',
-        'USER': 'u_eflowerlite',
-        'PASSWORD': 'J3r3m1ah',
+        'NAME': get_env_variable("DB_NAME"),
+        'USER': get_env_variable("DB_USER"),
+        'PASSWORD': get_env_variable("DB_PASSWORD"),
         'HOST': 'localhost',
         'PORT': '',
     }
