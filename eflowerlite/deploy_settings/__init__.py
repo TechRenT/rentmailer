fe12e7fda1b723d74e5fe41bcbc2a099b86df70a
Renelle Tigue <titingrenelle@gmail.com>
2017-04-02 15:24:37 +0800

    set postgresql as default database

--- eflowerlite/deploy_settings/__init__.py
+++ eflowerlite/deploy_settings/__init__.py
@@ -9,3 +9,14 @@ ALLOWED_HOSTS = [
 ]
 
 SECRET_KEY = get_env_variable("SECRET_KEY")
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'eflowerlite_prod',
+        'USER': 'u_eflowerlite',
+        'PASSWORD': 'J3r3m1ah',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
